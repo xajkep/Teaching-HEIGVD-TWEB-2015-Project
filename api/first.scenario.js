90ed4caa949a9a8749cb1e9fe96dfae32f7814df
Thibault Schowing <thibault.schowing@heig-vd.ch>
2015-10-09 15:35:24 +0200

    First scenario with copilot api

---
+++ api/first.scenario.js
@@ -0,0 +1,47 @@
+var _ = require('underscore'), copilot = require('api-copilot');
+
+
+// create an API scenario
+var scenario = new copilot.Scenario({
+  name: 'Premier Scenario',
+  summary: 'peupler l\'API avec des données',
+  baseUrl: 'localhost/api',
+  defaultRequestOptions: {
+    json: true
+  }
+});
+
+// Définition des étapes (steps)
+scenario.step('register a user', function() {
+  // make HTTP calls
+  return this.post({
+    url: '/account',
+    body: {
+      firstname: 'Paul',
+      lastname: 'Finch',
+      email: 'asdf@asdf.asdf',
+      password: 'changeme'
+    },
+    expect: {statusCode: 200}
+  });
+});
+
+// each step gets the result from the previous step
+
+scenario.step('login with previous info', function(response){
+  return this.post({
+    url:'/account',
+    body: {
+      email: 'asdf@asdf.asdf',
+      password: 'changeme'
+    },
+    expect: { statusCode: 200}
+  });
+});
+
+
+
+
+
+// Export the scenario for API Copilot to use
+module.exports = scenario;
